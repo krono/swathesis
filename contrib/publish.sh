a3e6f89e13cfbb905bad241a5e3e6b5ed3defbb0
Tobias Pape <tobias@netshed.de>
2015-07-09 16:17:25 +0200

    The nice thesis progress generators.
    TODO: Document :)

--- contrib/publish.sh
+++ contrib/publish.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
 
+###################################################
+# CHANGE ACCORDINGLY
+###################################################
+
 FILE=thesis
 PDF_FILE=${FILE}.pdf
 TEX_FILE=${FILE}.tex
 
+
+RESEARCHER_NAME=CHANGEME
+RESEARCHER_KIND=Researcher
+
+FIRST_DAY=2012-01-01
+EXPECTED_END=2012-06-30
+
+
 PUBLIC="${PUBLIC:-/CHANGEME/Dropbox/Public/thesis}"
+
+# PRONOUN
+HOW_I_ROLL=she
+# POSSESIVE
+HOW_I_HIT=her
+# OBJECTIVE (her/him/them)
+HOW_I_HOLLA=her
+
+###################################################
+# (That should be it)
+###################################################
+
+
+TARGET_WORDS=25000
+TARGET_PAGES=80
+
 JSON="${PUBLIC}/data.js"
 WORDS="${PUBLIC}/words.part"
 PAGES="${PUBLIC}/pages.part"
+
 
 if [ ! -f ${PDF_FILE} ];
 then
@@ -18,6 +47,8 @@ fi
 # publish stuff
 
 DATE=$(date +%s)
+START_DATE=$(date -j -f "%Y-%m-%d|%H:%M:%S" "${FIRST_DAY}|00:00:00" "+%s")
+END_DATE=$(date -j -f "%Y-%m-%d|%H:%M:%S" "${EXPECTED_END}|23:59:59" "+%s")
 
 PDFTOTEX_COUNT_1=$(pdftotext  -nopgbrk ${PDF_FILE} - | wc -w)
 PDFTOTEX_COUNT_2=$(pdftotext -raw -nopgbrk ${PDF_FILE} - | wc -w)
@@ -31,15 +62,29 @@ PAGE_COUNT=$(pdfinfo ${PDF_FILE} | grep Pages | awk '{ printf "%s", $2 }')
 echo "{x: ${DATE}, y: ${WORD_COUNT_MEAN} }," >> ${WORDS}
 echo "{x: ${DATE}, y: ${PAGE_COUNT} }," >> ${PAGES}
 
+# OUT
+echo ";" >${JSON}
+
 # words
-echo "var words = [" > ${JSON}
+echo "var words = [" >>${JSON}
 cat ${WORDS} >> ${JSON}
-echo "]" >> ${JSON}
+echo "];" >> ${JSON}
+echo "var targetWords = ${TARGET_WORDS};" >> ${JSON}
 
 # pages
 echo "var pages = [" >> ${JSON}
 cat ${PAGES} >> ${JSON}
-echo "]" >> ${JSON}
+echo "];" >> ${JSON}
+echo "var targetPages = ${TARGET_PAGES};" >> ${JSON}
+
+# dates, names n stuff
+echo "var firstDay = ${START_DATE};" >> ${JSON}
+echo "var expectedEnd = ${END_DATE};" >> ${JSON}
+echo "var theName = '${RESEARCHER_NAME}';"  >> ${JSON}
+echo "var theGame = '${RESEARCHER_KIND}';"  >> ${JSON}
+echo "var theThing = '${HOW_I_ROLL}';" >> ${JSON}
+echo "var theOwn = '${HOW_I_HIT}';" >> ${JSON}
+echo "var theStuff = '${HOW_I_HOLLA}';" >> ${JSON}
 
 # thumbnails
 convert thesis.pdf -alpha off -resize '60' ${PUBLIC}/images/thumbnail.png
@@ -49,4 +94,5 @@ EXIT=$?
 echo "There are ${WORD_COUNT_MEAN}  (${PDFTOTEX_COUNT}, ${DETEX_COUNT}) on ${PAGE_COUNT} pages"
 
 exit $EXIT;
+# I hate JavaScript
 
