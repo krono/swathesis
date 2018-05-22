25c3b2f02d368d15697bf26ed7aaa54b6eeac122
Tobias Pape <tobias@netshed.de>
2018-05-22 14:44:45 +0200

    fix name

--- requirements/get_requirements.sh
+++ requirements/get_requirements.sh
@@ -255,7 +255,7 @@ fi
 if _need_package "newpx" "2017/08/18"; then
   TDS=`_get_tds "newpx" ""`
   _deploy_tds $TDS
-  $ECHO ">> installed current acronym"
+  $ECHO ">> installed current newpx"
 fi
 
 if _need_package "ebgaramond" "2013/05/22"; then
